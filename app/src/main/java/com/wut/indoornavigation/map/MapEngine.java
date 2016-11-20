75aec027c5a67a780306d1fe79732a2c826791e5
Pawel <pawel.duszak@qualent.eu>
2016-11-20 21:19:38 +0100

    removing comment

--- app/src/main/java/com/wut/indoornavigation/map/MapEngine.java
+++ app/src/main/java/com/wut/indoornavigation/map/MapEngine.java
@@ -6,7 +6,7 @@ import com.wut.indoornavigation.data.model.BuildingData;
 
 public interface MapEngine {
 
-    void renderMap(@NonNull BuildingData someMapInfo); // TODO: 17.11.2016 Need to be changed
+    void renderMap(@NonNull BuildingData someMapInfo);
 
     void setOnMapReadyListener(OnMapReadyListener onMapReadyListener);
 }
