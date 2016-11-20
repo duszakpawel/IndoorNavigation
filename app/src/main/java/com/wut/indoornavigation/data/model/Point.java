a98d8f164dd2cab38b6716a46efe66a8f380702b
Pawel <pawel.duszak@qualent.eu>
2016-11-20 22:42:35 +0100

    location approximator

--- app/src/main/java/com/wut/indoornavigation/data/model/Point.java
+++ app/src/main/java/com/wut/indoornavigation/data/model/Point.java
@@ -4,7 +4,6 @@ import lombok.Value;
 
 @Value
 public class Point {
-
-    int x;
-    int y;
+    float x;
+    float y;
 }
---
+++ app/src/main/java/com/wut/indoornavigation/data/model/Signal.java
@@ -0,0 +1,9 @@
+package com.wut.indoornavigation.data.model;
+
+/**
+ * Created by Pawel on 20.11.2016.
+ */
+
+public class Signal {
+    public float Strength;
+}
