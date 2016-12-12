fd31caf97de144d4f00a0611292d64f32f987031
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2016-12-12 11:53:28 +0100

    [map] Add photo view

--- app/src/main/java/com/wut/indoornavigation/presenter/map/fragment/MapFragmentContract.java
+++ app/src/main/java/com/wut/indoornavigation/presenter/map/fragment/MapFragmentContract.java
@@ -1,15 +1,19 @@
 package com.wut.indoornavigation.presenter.map.fragment;
 
+import android.graphics.Bitmap;
+
 import com.hannesdorfmann.mosby.mvp.MvpPresenter;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
 public interface MapFragmentContract {
 
     interface View extends MvpView {
-
+        void showMap(Bitmap bitmap);
     }
 
     interface Presenter extends MvpPresenter<View> {
+        String[] getFloorSpinnerData();
 
+        void floorSelected(int position);
     }
 }
