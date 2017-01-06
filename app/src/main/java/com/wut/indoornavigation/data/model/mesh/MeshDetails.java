f252c32953ea8213b464b650ddfdf3d9c1d53ecd
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2017-01-06 18:12:16 +0100

    Add loading text to splash screen

--- app/src/main/java/com/wut/indoornavigation/data/model/mesh/MeshDetails.java
+++ app/src/main/java/com/wut/indoornavigation/data/model/mesh/MeshDetails.java
@@ -3,11 +3,10 @@ package com.wut.indoornavigation.data.model.mesh;
 import com.wut.indoornavigation.data.model.Point;
 import com.wut.indoornavigation.data.model.graph.Vertex;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.Value;
 
 /**
@@ -16,12 +15,18 @@ import lombok.Value;
  * also passages and beacons as points map).
  */
 @Value
-@NoArgsConstructor
-@AllArgsConstructor
 public class MeshDetails {
     Map<Integer, List<Vertex>> destinationVerticesDict;
     Map<Integer, List<Vertex>> elevatorsVerticesDict;
     Map<Integer, List<Vertex>> stairsVerticesDict;
     Map<Integer, List<Point>> passageVerticesDict;
     Map<Integer, List<Point>> beaconsDict;
+
+    public MeshDetails() {
+        destinationVerticesDict = new HashMap<>();
+        elevatorsVerticesDict = new HashMap<>();
+        stairsVerticesDict = new HashMap<>();
+        passageVerticesDict = new HashMap<>();
+        beaconsDict = new HashMap<>();
+    }
 }
