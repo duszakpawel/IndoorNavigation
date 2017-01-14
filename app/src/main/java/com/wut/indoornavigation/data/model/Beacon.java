68bdc00de7ad59c012cf9196238abf8f72f7c26c
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2017-01-14 12:00:46 +0100

    Fix setters

--- app/src/main/java/com/wut/indoornavigation/data/model/Beacon.java
+++ app/src/main/java/com/wut/indoornavigation/data/model/Beacon.java
@@ -1,20 +1,24 @@
 package com.wut.indoornavigation.data.model;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.Value;
 
 /**
  * Class representing beacon
  */
-@Value
+@Getter
 @Setter
+@ToString
 @EqualsAndHashCode(callSuper = true)
 public class Beacon extends BuildingObject {
 
     int major;
     int minor;
     int rssi;
+
     public Beacon(int id, int x, int y, int major, int minor) {
         super(id, x, y);
         this.major = major;
